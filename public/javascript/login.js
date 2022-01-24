9dd7888a296077b849bbeed6786f469e7eee7b25
Isaaccna <isaac.andrade1231@gmail.com>
2022-01-23 19:46:23 -0500

    everything is working: partials were relocated, relationships between the models fixed

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -43,7 +43,7 @@
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
